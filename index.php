755c184dbefba491187a5e67efc08ed3d56e9031
Pablo <data250@gmail.com>
2016-01-20 17:47:00 +0100

    Odczyt "po stronie"

--- index.php
+++ index.php
@@ -11,37 +11,47 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        
-        
         include_once('simple_html_dom.php'); 
+        session_start();  
+
+        if (!isset($_SESSION['pgs'])) { 
+            $_SESSION['pgs'] = -1;      
+        } else {                          
+            //$_SESSION['pgs']--;        
+        }
+        
         $ceneo = 'http://www.ceneo.pl/';
         $kod = 27472709;
         $adres = $ceneo . $kod;
-        
-        //Badanie ilości stron
-        $html = file_get_html($adres); //tymczasowy adres
-        $reviewCount = $html->find('span[itemProp=reviewCount]');
-        foreach ($reviewCount as $rv) {
-            $reviews = $rv->innertext;
-        }
-        $pages = $reviews/10;
-        
-        
-        for($i=1;$i<$pages+1;$i++)
-        {
-            echo $i;
-          $html = file_get_html($adres . '/opinie-' . $i);
-          $nicks = $html->find('div.product-reviewer');
-          foreach ($nicks as $nick) {
-            echo $nick->innertext;
-            
-        }
-        unset($html, $nicks);
-          
-        }
-        
      
+        echo "Zmienna:".$_SESSION['pgs']."<br>";
         
+        if ($_SESSION['pgs'] == -1) 
+            {
+
+                //Badanie ilości stron
+                $html = file_get_html($adres); //tymczasowy adres
+                $reviewCount = $html->find('span[itemProp=reviewCount]');
+                foreach ($reviewCount as $rv) {
+                    $reviews = $rv->innertext;
+                }
+                $pages = $reviews/10;
+                $_SESSION['pgs']=$pages;
+        } elseif ($_SESSION['pgs'] == 0)
+        {
+            echo "Zakończono";
+        } else
+        {
+            
+            $html = file_get_html($adres . '/opinie-' . $_SESSION['pgs']);
+            $nicks = $html->find('div.product-reviewer');
+            foreach ($nicks as $nick) {
+                echo $nick->innertext;
+            }
+            $_SESSION['pgs']--;
+            header('refresh: 1;');
+        }
+
         ?>
     </body>
 </html>
